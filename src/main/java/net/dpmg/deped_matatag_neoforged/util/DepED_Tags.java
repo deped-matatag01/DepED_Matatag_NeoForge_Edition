f99e5e7a24d1232a49cc432ca324825b653e9836
deped-matatag01 <g3-241-00633@gma.uphsl.edu.ph>
2025-05-09 21:44:54 +0800

    COMMIT CHANGES

---
+++ src/main/java/net/dpmg/deped_matatag_neoforged/util/DepED_Tags.java
@@ -0,0 +1,29 @@
+package net.dpmg.deped_matatag_neoforged.util;
+
+import net.dpmg.deped_matatag_neoforged.DepEDMatatagNeoForgeEdition;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+
+public class DepED_Tags {
+    public static class Blocks {
+
+
+
+        private static TagKey<Block> createTag(String name) {
+            return BlockTags.create(ResourceLocation.fromNamespaceAndPath(DepEDMatatagNeoForgeEdition.MOD_ID, name));
+        }
+    }
+
+    public static class Items {
+
+
+
+        private static TagKey<Item> createTag(String name) {
+            return ItemTags.create(ResourceLocation.fromNamespaceAndPath(DepEDMatatagNeoForgeEdition.MOD_ID, name));
+        }
+    }
+}
